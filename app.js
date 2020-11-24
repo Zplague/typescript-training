86d5c5897bb9a9cab06f2e56dfbdabae8a37786b
arodriguez <zplaguee@gmail.com>
2020-11-23 19:43:25 -0500

    capitalizar nombreHeroes

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-console.log('Hola Mundoo original !!!!!!');
+console.log("Hola Mundoo original !!!!!!");
 var esSuperman = true;
 var esBatman;
 var esAquaman = true;
 if (esSuperman) {
-    console.log('Estamos salvados');
+    console.log("Estamos salvados");
 }
 else {
-    console.log('moriremos ');
+    console.log("moriremos ");
 }
-// orden del if segun la asignacion nueva para 
+// orden del if segun la asignacion nueva para
 //esSuperman varia el resultado
 esSuperman = convertirClark();
 if (esSuperman) {
-    console.log('Estamos salvados');
+    console.log("Estamos salvados");
 }
 else {
-    console.log('moriremos ');
+    console.log("moriremos ");
 }
 function convertirClark() {
     return false;
@@ -24,15 +24,15 @@ var villanos = 1;
 var avengers = 5;
 var otros = 2;
 if (avengers > villanos) {
-    console.log('Estamos Salvadoss todos');
+    console.log("Estamos Salvadoss todos");
 }
 else {
-    console.log('Moriremos todos');
+    console.log("Moriremos todos");
 }
 // Strings + concat
-var flash = 'batman';
-var linternaVerde = 'linterna Verde';
-var volcanNegro = 'volcan Negro';
+var flash = "batman";
+var linternaVerde = "linterna Verde";
+var volcanNegro = "volcan Negro";
 console.log(flash);
 console.log(linternaVerde);
 console.log(volcanNegro);
@@ -42,7 +42,7 @@ console.log(concat);
 var vengador;
 var existee;
 var derrotas;
-vengador = 'Dr Strange';
+vengador = "Dr Strange";
 console.log(vengador.charAt(0));
 vengador = 12332.222;
 console.log(vengador.toFixed());
@@ -50,9 +50,9 @@ vengador = true;
 console.log(vengador.valueOf());
 // Arreglo - Array
 var arreglo = [1, 2, 3, 4, 5, 6];
-var malos = ['Dormamu', 'Ultron', 'Thanos'];
+var malos = ["Dormamu", "Ultron", "Thanos"];
 // Tuplas - Tuple Arreglo de varios tipos de dato
-var tupla = ['Andres', 31, true];
+var tupla = ["Andres", 31, true];
 console.log(tupla);
 // Enumeraciones
 // Acepta operaciones matematicas entre las mismas declaraciones
@@ -76,67 +76,68 @@ console.log(Volumen);
 error('Error critico..');
 */
 // Asersion de tipo de valor
-var cualquierValor = 'Cualquier cosa acá';
+var cualquierValor = "Cualquier cosa acá";
 var largoString = cualquierValor.length;
 console.log(largoString);
 //Funciones
-var heroe = 'Flash';
+var heroe = "Flash";
 function imprime_heroe() {
     return heroe;
 }
 var batisenal = function () {
-    return 'Batiseñal activada.';
+    return "Batiseñal activada.";
 };
 console.log(imprime_heroe());
 console.log(batisenal());
 // Parametros obligatorios en funciones
 function datos(nombre, apellido, edad) {
-    return nombre + ' ' + apellido + ' ' + edad;
+    return nombre + " " + apellido + " " + edad;
 }
-var userInfo = datos('Andres', 'Rodriguez', 31);
+var userInfo = datos("Andres", "Rodriguez", 31);
 console.log(userInfo);
 // Parametros opcionales en funciones, se utiliza el simbolo '?' para hacer un parametro opcional en la funcion
 function nombreCompleto(nombre, apellido) {
     if (apellido) {
-        return nombre + ' ' + apellido;
+        return nombre + " " + apellido;
     }
     else {
         return nombre;
     }
 }
-var nombre = nombreCompleto('Andres');
+var nombre = nombreCompleto("Andres");
 console.log(nombre);
 // Parametros por defecto en funciones. Se utiliza para parametrizar algunos aspectos como estandar.
 function miNombre(nombre, apellido, capitalizado) {
     if (capitalizado === void 0) { capitalizado = true; }
     if (capitalizado) {
-        return capitalizar(nombre) + ' ' + capitalizar(apellido);
+        return capitalizar(nombre) + " " + capitalizar(apellido);
     }
     else {
-        return nombre + ' ' + apellido;
+        return nombre + " " + apellido;
     }
 }
 function capitalizar(palabra) {
     return palabra.charAt(0).toUpperCase() + palabra.substr(1).toLowerCase();
 }
-var nombreCapitalizado = miNombre('andres', 'daniel');
+var nombreCapitalizado = miNombre("andres", "daniel");
 console.log(nombreCapitalizado);
 // Parametros REST. deMasParametros se utiliza para recibir x cantidad de parametros restantes y poderlos manejar como sean necesarios.
-// En este ejemplo se recogen y se concatenan 
+// En este ejemplo se recogen y se concatenan
 function nombreHeroes(nombre) {
     var deMasParametros = [];
     for (var _i = 1; _i < arguments.length; _i++) {
         deMasParametros[_i - 1] = arguments[_i];
     }
+    var nuevosParametros = [];
     for (var _a = 0, deMasParametros_1 = deMasParametros; _a < deMasParametros_1.length; _a++) {
         var parts = deMasParametros_1[_a];
-        var capitalized = parts.charAt(0).toUpperCase() + parts.substr(1).toLowerCase();
-        console.log(capitalized);
+        var capitalized = capitalizar(parts);
+        nuevosParametros.push(capitalized);
     }
-    return capitalizar(nombre) + ' ' + deMasParametros.join(' ');
+    return capitalizar(nombre) + " " + nuevosParametros.join(" ");
 }
-var superMan = nombreHeroes('clark', 'joseph', 'kent', 'daniel');
-var ironMan = nombreHeroes('tony', 'antonio', 'stark');
+var superMan = nombreHeroes("clark", "joseph", "kent", "daniel");
+var ironMan = nombreHeroes("tony", "antonio", "stark");
 console.log(superMan);
 console.log(ironMan);
 // Parametros - Filtrado para variables tipo Funcion. Se utiliza para restringir una variable a un solo tipo de funcion, ya sea numero, string etc.
@@ -147,7 +148,7 @@ function saludar(nombre) {
     return "I'm " + nombre;
 }
 function salvarMundo() {
-    console.log('Mundo a salvo');
+    console.log("Mundo a salvo");
 }
 // Asignacion de tipo de parametro Numero para la funcion, se pueden colocar cualquier tipo de valor para alterar.
 var myFunction;
@@ -160,4 +161,4 @@ console.log( myFunction('Andres')); */
 myFunction() */
 var myFunction2;
 myFunction2 = saludar;
-console.log(myFunction2('asd'));
+console.log(myFunction2("asd"));
--- app.ts
+++ app.ts
@@ -1,53 +1,53 @@
-console.log('Hola Mundoo original !!!!!!')
+console.log("Hola Mundoo original !!!!!!");
 
-let esSuperman:boolean = true;
-let esBatman:boolean;
-let esAquaman:boolean = true;
+let esSuperman: boolean = true;
+let esBatman: boolean;
+let esAquaman: boolean = true;
 
 if (esSuperman) {
-    console.log('Estamos salvados')
+  console.log("Estamos salvados");
 } else {
-    console.log('moriremos ')
+  console.log("moriremos ");
 }
 
-// orden del if segun la asignacion nueva para 
+// orden del if segun la asignacion nueva para
 //esSuperman varia el resultado
 
-esSuperman= convertirClark()
+esSuperman = convertirClark();
 
 if (esSuperman) {
-    console.log('Estamos salvados')
+  console.log("Estamos salvados");
 } else {
-    console.log('moriremos ')
-} 
-
-function convertirClark(){
-    return  false 
+  console.log("moriremos ");
 }
 
-let villanos:number = 1;
-let avengers:number = 5;
+function convertirClark() {
+  return false;
+}
+
+let villanos: number = 1;
+let avengers: number = 5;
 let otros = 2;
 
 if (avengers > villanos) {
-    console.log('Estamos Salvadoss todos');
+  console.log("Estamos Salvadoss todos");
 } else {
-    console.log('Moriremos todos')
+  console.log("Moriremos todos");
 }
 
 // Strings + concat
 
-let flash:string = 'batman';
-let linternaVerde:string = 'linterna Verde'
-let volcanNegro:string = 'volcan Negro'
+let flash: string = "batman";
+let linternaVerde: string = "linterna Verde";
+let volcanNegro: string = "volcan Negro";
 
-console.log(flash)
-console.log(linternaVerde)
-console.log(volcanNegro)
+console.log(flash);
+console.log(linternaVerde);
+console.log(volcanNegro);
 
-let concat:string = `Los super heroes son: ${flash}, ${linternaVerde}, ${volcanNegro}`
+let concat: string = `Los super heroes son: ${flash}, ${linternaVerde}, ${volcanNegro}`;
 
-console.log(concat)
+console.log(concat);
 
 // Tipo ANY
 
@@ -55,170 +55,166 @@ let vengador;
 let existee;
 let derrotas;
 
-vengador = 'Dr Strange'
+vengador = "Dr Strange";
 
-console.log( vengador.charAt(0) )
+console.log(vengador.charAt(0));
 
-vengador = 12332.222
+vengador = 12332.222;
 
-console.log(vengador.toFixed())
+console.log(vengador.toFixed());
 
-vengador = true 
+vengador = true;
 
-console.log(vengador.valueOf()) 
-
+console.log(vengador.valueOf());
 
 // Arreglo - Array
 
-let arreglo:number[] = [1,2,3,4,5,6];
+let arreglo: number[] = [1, 2, 3, 4, 5, 6];
 
-let malos:string[] = ['Dormamu','Ultron','Thanos']
+let malos: string[] = ["Dormamu", "Ultron", "Thanos"];
 
 // Tuplas - Tuple Arreglo de varios tipos de dato
 
-let tupla: [string,number,boolean] = ['Andres', 31, true ]
+let tupla: [string, number, boolean] = ["Andres", 31, true];
 
-console.log(tupla)
+console.log(tupla);
 
 // Enumeraciones
 // Acepta operaciones matematicas entre las mismas declaraciones
 // pero respeta el orden en el que estan.
 
-enum Volumen{
-    min = 1,
-    max = 120,
-    medio = max / 2
+enum Volumen {
+  min = 1,
+  max = 120,
+  medio = max / 2,
 }
 
-let audio:number = Volumen.medio
+let audio: number = Volumen.medio;
 
-console.log(audio)
+console.log(audio);
 
-console.log(Volumen)
+console.log(Volumen);
 
 // Tipo Never, utilizar para destacar un punto que el codigo nunca debe alcanzar.
 /* function error(mensaje:string):never {
 
     throw new Error(mensaje);
 
-} 
+}
 
 error('Error critico..');
 */
 
 // Asersion de tipo de valor
 
-let cualquierValor:any = 'Cualquier cosa acá'
+let cualquierValor: any = "Cualquier cosa acá";
 
-let largoString:number = (<string>cualquierValor).length
+let largoString: number = (<string>cualquierValor).length;
 
-console.log(largoString)
-
+console.log(largoString);
 
 //Funciones
 
-let heroe:string = 'Flash';
+let heroe: string = "Flash";
 
-function imprime_heroe():string{
-    return heroe
+function imprime_heroe(): string {
+  return heroe;
 }
 
-let batisenal = function():string {
-    return 'Batiseñal activada.';
-}
+let batisenal = function (): string {
+  return "Batiseñal activada.";
+};
 
-console.log(imprime_heroe())
-console.log(batisenal())
+console.log(imprime_heroe());
+console.log(batisenal());
 
 // Parametros obligatorios en funciones
 
-function datos( nombre:string, apellido:string, edad:number):string{
-    return nombre + ' ' + apellido + ' ' + edad
+function datos(nombre: string, apellido: string, edad: number): string {
+  return nombre + " " + apellido + " " + edad;
 }
 
-let userInfo = datos('Andres','Rodriguez', 31)
+let userInfo = datos("Andres", "Rodriguez", 31);
 
-console.log(userInfo)
+console.log(userInfo);
 
 // Parametros opcionales en funciones, se utiliza el simbolo '?' para hacer un parametro opcional en la funcion
 
-function nombreCompleto( nombre:string, apellido?:string ):string {
-    if (apellido) {
-        return nombre + ' ' + apellido
-    }else {
-        return nombre
-    }
+function nombreCompleto(nombre: string, apellido?: string): string {
+  if (apellido) {
+    return nombre + " " + apellido;
+  } else {
+    return nombre;
+  }
 }
 
-let nombre = nombreCompleto('Andres');
+let nombre = nombreCompleto("Andres");
 
-console.log(nombre)
+console.log(nombre);
 
 // Parametros por defecto en funciones. Se utiliza para parametrizar algunos aspectos como estandar.
 
 function miNombre(
-    nombre:string, apellido:string, capitalizado:boolean = true){
-
-        if (capitalizado) {
-            return capitalizar(nombre) + ' ' + capitalizar(apellido);
-        }else{
-            return nombre + ' ' + apellido;
-        }
-    }
-
-function capitalizar(palabra:string):string{
-    return palabra.charAt(0).toUpperCase() + palabra.substr(1).toLowerCase();
+  nombre: string,
+  apellido: string,
+  capitalizado: boolean = true
+) {
+  if (capitalizado) {
+    return capitalizar(nombre) + " " + capitalizar(apellido);
+  } else {
+    return nombre + " " + apellido;
+  }
 }
 
-var nombreCapitalizado = miNombre('andres', 'daniel')
+function capitalizar(palabra: string): string {
+  return palabra.charAt(0).toUpperCase() + palabra.substr(1).toLowerCase();
+}
+
+var nombreCapitalizado = miNombre("andres", "daniel");
 
 console.log(nombreCapitalizado);
 
 // Parametros REST. deMasParametros se utiliza para recibir x cantidad de parametros restantes y poderlos manejar como sean necesarios.
-// En este ejemplo se recogen y se concatenan 
+// En este ejemplo se recogen y se concatenan
 
-function nombreHeroes(nombre:string, ...deMasParametros:string[]):string{
-    
-        for (let parts of deMasParametros) {
-            var capitalized:string = parts.charAt(0).toUpperCase() + parts.substr(1).toLowerCase()
-            
-            console.log(capitalized)
-        }
+function nombreHeroes(nombre: string, ...deMasParametros: string[]): string {
+  let nuevosParametros: string[] = [];
 
-        return capitalizar(nombre) + ' ' + deMasParametros.join(' ')
-    }
-    
-    let superMan = nombreHeroes('clark','joseph','kent','daniel')
-    let ironMan = nombreHeroes('tony','antonio','stark')
+  for (let parts of deMasParametros) {
+    let capitalized = capitalizar(parts);
+    nuevosParametros.push(capitalized);
+  }
 
-    console.log(superMan)
-    console.log(ironMan)
-    
+  return capitalizar(nombre) + " " + nuevosParametros.join(" ");
+}
 
+let superMan = nombreHeroes("clark", "joseph", "kent", "daniel");
+let ironMan = nombreHeroes("tony", "antonio", "stark");
 
-
+console.log(superMan);
+console.log(ironMan);
 
 // Parametros - Filtrado para variables tipo Funcion. Se utiliza para restringir una variable a un solo tipo de funcion, ya sea numero, string etc.
 
-function suma(a:number,b:number){
-    return a + b;
+function suma(a: number, b: number) {
+  return a + b;
 }
 
-function saludar(nombre:string):string{
-    return "I'm " + nombre;
+function saludar(nombre: string): string {
+  return "I'm " + nombre;
 }
 
-function salvarMundo():void{
-    console.log('Mundo a salvo')
+function salvarMundo(): void {
+  console.log("Mundo a salvo");
 }
 
 // Asignacion de tipo de parametro Numero para la funcion, se pueden colocar cualquier tipo de valor para alterar.
-let myFunction: (a:number,b:number) => number;
+let myFunction: (a: number, b: number) => number;
 
 //myFunction = 10;
 
 myFunction = suma;
-console.log( myFunction(2,2));
+console.log(myFunction(2, 2));
 
 /* myFunction = saludar;
 console.log( myFunction('Andres')); */
@@ -226,8 +222,7 @@ console.log( myFunction('Andres')); */
 /* myFunction = salvarMundo;
 myFunction() */
 
+let myFunction2: (n: string) => string;
 
-let myFunction2: (n:string) => string;
-
-myFunction2 = saludar
-console.log( myFunction2('asd'))
+myFunction2 = saludar;
+console.log(myFunction2("asd"));
